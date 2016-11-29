edc7c069fd2cf888aebd2c9915acd2dd040f8d67
Chris Carter <chris.carter@unboxedconsulting.com>
2016-11-29 14:32:04 +0000

    Initial commit with data

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+gem "dotenv-rails", require: "dotenv/rails-now"
+
+gem "rails", "~> 5.0.0"
+gem "pg", "~> 0.15"
+
+gem "jquery-rails"
+gem "jbuilder", "~> 2.0"
+
+group :development, :test do
+  gem "pry"
+  gem "pry-byebug"
+end
+
---
+++ db/migrate/20161129092722_create_crimes.rb
@@ -0,0 +1,14 @@
+class CreateCrimes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :crimes do |t|
+      t.date :date, null: false
+      t.string :reported_by, null: false
+      t.string :falls_within, null: false
+      t.decimal :lat, null: true
+      t.decimal :lng, null: true
+      t.string :location, null: true
+      t.string :type
+      t.string :outcome
+    end
+  end
+end
