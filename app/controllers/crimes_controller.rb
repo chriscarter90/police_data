a5b5e3d70dc1f62f12022028f9edb1b08d936be8
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2016-11-30 11:19:42 +0000

    WIP

---
+++ app/controllers/crimes_controller.rb
@@ -0,0 +1,17 @@
+class CrimesController < ApplicationController
+  def index
+    respond_to do |format|
+      if params['category']
+        @count = Crime.where(crime_type: params['category']).count
+      else
+        @count = Crime.count
+      end
+
+      format.html {
+        @categories = ["Other crime", "Possession of weapons", "Criminal damage and arson", "Shoplifting", "Robbery", "Burglary", "Drugs", "Other theft", "Public order", "Violence and sexual offences", "Vehicle crime", "Bicycle theft", "Theft from the person", "Anti-social behaviour",]
+      }
+
+      format.js
+    end
+  end
+end
